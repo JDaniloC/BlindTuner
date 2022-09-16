df02730a75332515c3e743be027cc1b31da1ad48
JDaniloC <jdsc@cin.ufpe.br>
2022-09-15 22:52:29 -0300

    #3 add a sampler: there's a bug It's not possible to play fast even with tone Buffer. Maybe changing the frequency can resolve

--- blindturner/screens/TabOneScreen.tsx
+++ blindturner/screens/TabOneScreen.tsx
@@ -1,30 +1,40 @@
-import { Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 import * as Tone from 'tone'
 import React from 'react';
-import { useEffect } from 'react';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   const [value, setValue] = React.useState(110);
-  const synth = new Tone.Synth().toDestination();
 
-  useEffect(() => {
-    const now = Tone.now()
-    synth.triggerAttackRelease(value, '8n', now);
-  }, [value]);
+  const sampler = new Tone.Sampler({
+          urls: {
+            C4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/C4.mp3"),
+            D4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/D4.mp3"),
+            E4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/E4.mp3"),
+            F4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/F4.mp3"),
+            G4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/G4.mp3"),
+            A4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/A4.mp3"),
+            B4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/B4.mp3"),
+          },
+          release: 5,
+      }).toDestination();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         ToneJS testing
       </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.separator} lightColor="#eee"
+            darkColor="rgba(255,255,255,0.1)" />
       <Text>{value}</Text>
-      <input type="range" value={value} min={110} max={1661.21} step={1}
-          onChange={(evt) => {setValue(parseInt(evt.target.value))}}/>
+      <input type="range" value={value} min={262} max={494} step={1}
+          onChange={(evt) => {
+            setValue(parseInt(evt.target.value))
+            sampler.triggerAttackRelease(evt.target.value, '8n');
+          }}/>
     </View>
   );
 }
