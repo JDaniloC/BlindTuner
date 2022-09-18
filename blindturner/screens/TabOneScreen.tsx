0b37afc2ac5e2b12f6521764335718a9936769f1
JDaniloC <jdsc@cin.ufpe.br>
2022-09-17 23:05:43 -0300

    FIx sampler sound #3 Miss the holding feature

--- blindturner/screens/TabOneScreen.tsx
+++ blindturner/screens/TabOneScreen.tsx
@@ -6,20 +6,40 @@ import { RootTabScreenProps } from '../types';
 import * as Tone from 'tone'
 import React from 'react';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  const [value, setValue] = React.useState(110);
+const baseURL = "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/"
+const notes = {
+  60: new Tone.Buffer(`${baseURL}/C4.mp3`),
+  61: new Tone.Buffer(`${baseURL}/Cs4.mp3`),
+  62: new Tone.Buffer(`${baseURL}/D4.mp3`),
+  63: new Tone.Buffer(`${baseURL}/Ds4.mp3`),
+  64: new Tone.Buffer(`${baseURL}/E4.mp3`),
+  65: new Tone.Buffer(`${baseURL}/F4.mp3`),
+  66: new Tone.Buffer(`${baseURL}/Fs4.mp3`),
+  67: new Tone.Buffer(`${baseURL}/G4.mp3`),
+  68: new Tone.Buffer(`${baseURL}/Gs4.mp3`),
+  69: new Tone.Buffer(`${baseURL}/A4.mp3`),
+  71: new Tone.Buffer(`${baseURL}/B4.mp3`),
+}
+const frequencies = {
+  262: "C4.mp3",
+  277: "Cs4.mp3",
+  294: "D4.mp3",
+  311: "Ds4.mp3",
+  330: "E4.mp3",
+  349: "F4.mp3",
+  370: "Fs4.mp3",
+  392: "G4.mp3",
+  415: "Gs4.mp3",
+  440: "A4.mp3",
+  494: "B4.mp3",
+}
 
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [value, setValue] = React.useState(262);
+  
   const sampler = new Tone.Sampler({
-          urls: {
-            C4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/C4.mp3"),
-            D4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/D4.mp3"),
-            E4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/E4.mp3"),
-            F4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/F4.mp3"),
-            G4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/G4.mp3"),
-            A4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/A4.mp3"),
-            B4: new Tone.Buffer("https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/cello/B4.mp3"),
-          },
-          release: 5,
+          urls: notes,
+          release: 2,
       }).toDestination();
 
   return (
